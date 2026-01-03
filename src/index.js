754972e053cc1bbdc6b78b9a5c20e8673a46a0f6
Kazak <strannik19742@mail.ru>
2026-01-03 22:19:04 +0300

    feat: start basic express server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import http from 'http';
+import app from './app.js';
+
+const PORT = 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
