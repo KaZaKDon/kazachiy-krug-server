579b81520ca9c898c6385e9053113711dad50ac4
Kazak <strannik19742@mail.ru>
2026-01-04 11:31:50 +0300

    feat: add join chat via socket.io rooms

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,23 @@ export default function initSocket(io) {
     io.on('connection', (socket) => {
         console.log('Client connected:', socket.id);
 
+        socket.on('ping', (data) => {
+            console.log('Ping received from client:', data);
+
+            socket.emit('pong', {
+                message: 'pong from server',
+                time: Date.now(),
+            });
+        });
+
+        socket.on('join:chat', ({ chatId }) => {
+            console.log(`Socket ${socket.id} joined chat ${chatId}`);
+
+            socket.join(chatId);
+
+            socket.emit('chat:joined', { chatId });
+        });
+
         socket.on('disconnect', () => {
             console.log('Client disconnected:', socket.id);
         });
